36a9371474e46a85a478bc304f3dfb467dcdf55a
AyaJas <aya.hameedat@gmail.com>
2022-04-26 17:06:03 +0300

    first commit

---
+++ app/src/main/java/com/example/lab16/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.lab16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // this is a good place to do initial
+        // set up like click listeners etc
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: Called");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: called");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "onResume: called - The App is VISIBLE");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "onPause: called");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop: called");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy: called");
+        super.onDestroy();
+    }
+
+}
