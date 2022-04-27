15beb90ecd7d8ba23d72fd76523139ad02c16b1d
AyahHameedat <aya.hameedat@gmail.com>
2022-04-28 02:09:45 +0300

    lab26 is Done

--- app/src/main/java/com/example/lab16/MainActivity.java
+++ app/src/main/java/com/example/lab16/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,44 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private View.OnClickListener mAddClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            mMyTasks.setText("MY TASKS");
+            mMyTasks.setAllCaps(true);
+
+
+            Intent startAddTaskIntent = new Intent(getApplicationContext(), AddTask.class);
+
+            startAddTaskIntent.putExtra("ADD_TITLE", "Title");
+            startAddTaskIntent.putExtra("ADD_BODY", "Description");
+            startAddTaskIntent.putExtra("ADD_SUBMIT", "SUBMIT");
+//
+            Toast.makeText(getApplicationContext(), "submitted!", Toast.LENGTH_SHORT).show();
+
+            startActivity(startAddTaskIntent);
+            
+        }
+    };
+
+
+        private View.OnClickListener mAllClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            mAllTasks.setText("All TASKS");
+            mAllTasks.setAllCaps(true);
+
+            Intent startAllTasksIntent = new Intent(getApplicationContext(), AllTasks.class);
+            startActivity(startAllTasksIntent);
+
+        }
+    };
+    
+        private TextView mMyTasks;
+        private TextView mSubmit;
+        private TextView mAllTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +64,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: Called");
 
+
+        Button AddTaskButton = findViewById(R.id.ADD_TASK);
+        mMyTasks = findViewById(R.id.MY_TASKs);
+
+        AddTaskButton.setOnClickListener(mAddClickListener);
+
+
+//
+//        Button submitButton = findViewById(R.id.ADD_SUBMIT);
+//        mSubmit = findViewById(R.id.ADD_SUBMIT);
+////
+//        submitButton.setOnClickListener(mSubmitListener);
+
+
+
+        ////////////////*********ALL Task Button**********//////////////////
+
+        Button AllTaskButton = findViewById(R.id.ALL_TASKS);
+        mAllTasks = findViewById(R.id.ALL_TASKS);
+
+        AllTaskButton.setOnClickListener(mAllClickListener);
     }
 
     @Override
